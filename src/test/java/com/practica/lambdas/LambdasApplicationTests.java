0b17096aa408139043be09c086d3e3879edf9665
Daniel López <daniel.lopezing20@gmail.com>
2021-05-01 15:19:31 -0500

    Commit inicial.

---
+++ src/test/java/com/practica/lambdas/LambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.lambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
